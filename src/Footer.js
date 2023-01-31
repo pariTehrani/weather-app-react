210c7771fc89df27dc2c75d92c99b14f6cf75735
Pari Tehrani <lenzbequiet@gmail.com>
2023-01-31 22:48:39 +0330

    change link to github and netlify

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,8 @@ export default function Footer(){
         <div className="Footer">
             <footer className="footer mt-5">
       This project was coded by <a href="https://www.shecodes.io/graduates/61869-pari-tehrani">Pari Tehrani</a> and is 
-      <a href="https://github.com/pariTehrani/weather_app/tree/without_forecast" target="_blank" rel="noopener noreferrer">Open-sourced on GitHub</a> and hosted on
-      <a href="https://delicate-yeot-b4b0ed.netlify.app/">Netlify</a>
+      <a href="https://github.com/pariTehrani/weather-app-react/" target="_blank" rel="noopener noreferrer">Open-sourced on GitHub</a> and hosted on
+      <a href="https://cheerful-cannoli-187a06.netlify.app/">Netlify</a>
     </footer>
         </div>
     );
